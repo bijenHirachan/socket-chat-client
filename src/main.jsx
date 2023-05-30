654d3ec0a6dcbc2896b112ac95aab0a875374f04
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-05-30 18:44:02 +0200

    Server url updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import "./index.css";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
-export const server = "http://localhost:4000/api/v1";
+// export const server = "http://localhost:4000/api/v1";
+export const server = "https://socket-chat-server-p8u6.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:4000";
+// const URL = "http://localhost:4000";
+const URL = "https://socket-chat-server-p8u6.onrender.com";
 
 export const socket = io(URL, { autoConnect: false });
